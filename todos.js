0afbcf58f3a4f759ca1a10c115afaf18853088b1
larebear007 <larencozart@gmail.com>
2024-05-02 21:16:41 -0500

    Add flash messaging and validation/sanitization

--- todos.js
+++ todos.js
@@ -6,6 +6,10 @@ First, create a minimal Express program configured
 
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const TodoList = require("./lib/todolist.js");
+const {body, validationResult} = require("express-validator");
 
 const app = express();
 const host = "localhost";
@@ -18,6 +22,19 @@ app.set("view engine", "pug");
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
+app.use(session({
+  name: "launch-school-todos-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure"
+}));
 
 
 const sortTodoLists = lists => {
@@ -41,10 +58,64 @@ const sortTodoLists = lists => {
 };
 
 app.get("/", (req, res) => {
+  res.redirect("/lists");
+});
+
+app.get("/lists", (req, res) => {
   res.render("lists", {
     todoLists: sortTodoLists(todoLists)
   });
 });
+
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+// needs validation
+
+const validateTitle = (title) => {
+  return body(title)
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`The list title is required.`)
+    .bail()
+    .isLength({ max: 100 })
+    .withMessage(`The list title is too long. Maximum length is 100 characters.`)
+    .isAlpha()
+    .withMessage(`The list title contains invalid characters. Titles must be alphabetic.`);
+};
+
+
+app.post("/lists",
+  [
+    body("todoListTitle")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("The list title is required.")
+      .isLength({ max: 100 })
+      .withMessage("List title must be between 1 and 100 characters.")
+      .custom(title => {
+        let duplicate = todoLists.find(list => list.title === title);
+        return duplicate === undefined;
+      })
+      .withMessage("List title must be unique."),
+  ],
+  (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      errors.array().forEach(message => req.flash("error", message.msg));
+      res.render("new-list", {
+        flash: req.flash(),
+        todoListTitle: req.body.todoListTitle,
+      });
+    } else {
+      todoLists.push(new TodoList(req.body.todoListTitle));
+      req.flash("success", "The todo list has been created.");
+      res.redirect("/lists");
+    }
+  }
+);
+
 
 app.listen(port, host, () => {
   console.log(`Todos listening on ${port} ${host}...`);
